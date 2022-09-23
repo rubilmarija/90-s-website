6ada6010ec9f637516aa2f7009eac3e75bf844d6
Marija Rubil <rubilmarija@gmail.com>
2022-09-23 09:59:50 +0200

    Added new pages and fixed index.html

---
+++ script.js
@@ -0,0 +1,52 @@
+/*//let myName = 'Marija';
+
+//alert(myName);
+
+//document.getElementById('episodes').innerHTML = 'Explore episodes';
+//document.getElementById('episodes').style.backgroundColor = 'pink';
+
+
+let myElement = document.getElementById('episodes');
+
+myElement.classList.add('coolepisodes');
+//myElement.classList.remove('someotherclass');
+
+
+// Create element
+let myNewElement = document.createElement('h2');
+document.getElementById('episodes').appendChild(myNewElement);
+
+// Add text to element
+myNewElement.innerHTML = 'Hello h2';
+document.getElementById('episodes').appendChild(myNewElement);
+
+
+// FUNCTIONS
+
+function sayHello() {
+    alert('Hello');
+    alert('Hi');
+}
+
+sayHello();
+
+
+// Function that creates an element and append it to another element
+function addElement() {
+    let myNewElement = document.createElement('h2');
+    myNewElement.innerHTML = 'Hello h2';
+
+    document.getElementById('episodes').appendChild(myNewElement);
+}
+
+addElement();
+
+//EventListener
+let button = document.getElementById('button');
+button.addEventListener('click', addElement);
+
+
+function clickButton() {
+    alert('Button clicked');
+}
+*/
