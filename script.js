b27d54344e5477ab9d25d9b2d2ff5b71c6407dcc
Marija Rubil <rubilmarija@gmail.com>
2022-10-04 09:47:17 +0200

    Changed content

--- script.js
+++ script.js
@@ -50,3 +50,15 @@ function clickButton() {
     alert('Button clicked');
 }
 */
+var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+var mobileMenu = document.querySelector(".mobile-menu");
+
+mobileMenuBtn.addEventListener("click", () => {
+  if (mobileMenu.style.display === "none") {
+    mobileMenu.style.display = "flex";
+    mobileMenuBtn.innerHTML = "Close";
+  } else {
+    mobileMenu.style.display = "none";
+    mobileMenuBtn.innerHTML = "Menu";
+  }
+});
